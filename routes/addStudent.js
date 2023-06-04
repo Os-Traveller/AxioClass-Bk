1664e8e1b32f7d9a416533326b1c9718f0d1cf5e
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2023-06-05 01:15:52 +0600

    file changed

--- routes/addStudent.js
+++ routes/addStudent.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   console.log(req.body);
 });
 
