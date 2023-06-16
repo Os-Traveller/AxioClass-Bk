e58e7d46084155d4ebe35de2fae8662e29a49315
Os-Traveller <faisal.ahmed.20.35.197@gmail.com>
2023-06-16 14:32:17 +0600

    teacher added

---
+++ routes/addTeacher.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const teacherModel = require('../models/teacherModel');
+const { roundDigit } = require('../utils/helper');
+
+router.post('/', async (req, res) => {
+  const {
+    name,
+    dob,
+    birthPlace,
+    address,
+    email,
+    phone,
+    sex,
+    image,
+    education,
+    dept,
+  } = req.body;
+
+  // generating id
+  const count = await teacherModel.count({ dept });
+  const id = `T-${dept}-${roundDigit(count + 1, 2)}`;
+  const password = process.env.passwordSecret + phone;
+
+  try {
+    const newTeacher = await teacherModel.create({
+      id,
+      password,
+      image,
+      name,
+      dob,
+      birthPlace,
+      sex,
+      address,
+      dept,
+      education,
+      email,
+      phone,
+    });
+    res.send({ okay: true, data: newTeacher });
+  } catch (err) {
+    console.log(err.message);
+    return res.send({ okay: false, msg: err.message });
+  }
+});
+
+module.exports = router;
