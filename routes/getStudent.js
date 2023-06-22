232b0aaa84020834c518061f77f3b7f5463f4be2
Os-Traveller <faisal.ahmed.20.35.197@gmail.com>
2023-06-23 01:46:18 +0600

    student document api added

--- routes/getStudent.js
+++ routes/getStudent.js
@@ -1,71 +1,68 @@
 const express = require('express');
 const router = express.Router();
-const studentModel = require('../models/studentModel');
+const { studentsCollection } = require('../db/collections');
+// const studentModel = require('../models/studentModel');
 
 router.get('/:id', async (req, res) => {
   const id = req.params.id;
-  try {
-    const student = await studentModel.findOne({ id });
-    if (!student) return res.send({ okay: false, msg: 'Student not found' });
+  res.send({ msg: 'Wait' });
+  // try {
+  //   const student = await studentModel.findOne({ id });
+  //   if (!student) return res.send({ okay: false, msg: 'Student not found' });
 
-    const studentInfo = {
-      basicInfo: {
-        name: student.name,
-        dept: student.dept,
-        id: student.id,
-        intake: student.intake,
-        image: student.image,
-      },
-      financialInfo: {
-        demand: student.demand,
-        paid: student.paid,
-        waiver: student.waiver,
-        due: student.due,
-      },
-      academicInfo: {},
-      personalInfo: {
-        address: student.address,
-        phone: student.phone,
-        email: student.email,
-      },
-      guardianInfo: {
-        guardianName: student.guardianName,
-        guardianNumber: student.guardianNumber,
-      },
-      education: {
-        hsc: student.hsc,
-        ssc: student.ssc,
-      },
-    };
+  //   const studentInfo = {
+  //     basicInfo: {
+  //       name: student.name,
+  //       dept: student.dept,
+  //       id: student.id,
+  //       intake: student.intake,
+  //       image: student.image,
+  //     },
+  //     financialInfo: {
+  //       demand: student.demand,
+  //       paid: student.paid,
+  //       waiver: student.waiver,
+  //       due: student.due,
+  //     },
+  //     academicInfo: {},
+  //     personalInfo: {
+  //       address: student.address,
+  //       phone: student.phone,
+  //       email: student.email,
+  //     },
+  //     guardianInfo: {
+  //       guardianName: student.guardianName,
+  //       guardianNumber: student.guardianNumber,
+  //     },
+  //     education: {
+  //       hsc: student.hsc,
+  //       ssc: student.ssc,
+  //     },
+  //   };
 
-    res.send({ okay: true, data: studentInfo });
-  } catch (err) {
-    console.log(err);
-    res.send({ okay: false, msg: 'Something went wrong' });
-  }
+  //   res.send({ okay: true, data: studentInfo });
+  // } catch (err) {
+  //   console.log(err);
+  //   res.send({ okay: false, msg: 'Something went wrong' });
+  // }
 });
 
 router.get('/document/:id', async (req, res) => {
   const id = req.params.id;
-  try {
-    const student = await studentModel.findOne({ id });
-    if (!student) return res.send({ okay: false, msg: 'Student not found' });
+  const studentInfo = await studentsCollection.findOne({ id });
+  if (!studentInfo) return res.send({ okay: false, msg: 'No data found' });
 
-    const studentInfo = {
-      name: student.name,
-      dept: student.dept,
-      intake: student.intake,
-      id: student.id,
-      password: student.password,
-      image: student.image,
-      phone: student.phone,
-      email: student.email,
-    };
-    res.send({ okay: true, data: studentInfo });
-  } catch (err) {
-    console.log(err);
-    res.send({ okay: false, msg: 'something went wrong' });
-  }
+  const studentDocument = {
+    name: studentInfo.name,
+    dept: studentInfo.dept,
+    intake: studentInfo.intake,
+    id: studentInfo.id,
+    password: studentInfo.password,
+    image: studentInfo.image,
+    phone: studentInfo.phone,
+    email: studentInfo.email,
+  };
+  res.send({ okay: true, data: studentDocument });
 });
 
 module.exports = router;
