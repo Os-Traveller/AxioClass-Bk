b43319ea63c3f7cb30fb47ee277c71631f57c6b4
Os-Traveller <faisal.ahmed.20.35.197@gmail.com>
2023-07-09 00:57:57 +0600

    api added for class creation info

--- routes/classroom.js
+++ routes/classroom.js
@@ -3,6 +3,9 @@ const {
   classRoomCollection,
   activitiesCollection,
   studentsCollection,
+  teachersCollection,
+  coursesCollection,
+  departmentsCollection,
 } = require('../db/collections');
 const { getDateObject } = require('../utils/helper');
 const router = express.Router();
@@ -12,7 +15,9 @@ router.post('/add', async (req, res) => {
   const { dept, intake, courseCode } = classData;
 
   // finding students of that intake and dept
-  const studentsCursor = studentsCollection.find({ $and: [{ dept, intake }] });
+  const studentsCursor = studentsCollection.find({
+    $and: [{ dept, intake }],
+  });
   let studentList = await studentsCursor.toArray();
   if (!studentList) return res.send({ okay: false, msg: 'No Student found' });
 
@@ -39,13 +44,44 @@ router.post('/add', async (req, res) => {
   await activitiesCollection.insertOne({
     date: dateObject.date,
     activity: 'Created a classroom',
-    data: `Class Code : ${id}`,
+    data: `Class Code : ${classCode}`,
     time: dateObject.time,
   });
 
   if (!classCreateStatus.acknowledged)
     return res.send({ okay: false, msg: 'Could not create the class room' });
   res.send({ okay: true, classCode });
+});
+
+router.get('/creation-info/:dept/:intake', async (req, res) => {
+  try {
+    const { dept, intake } = req.params;
+    // getting all teacher list
+    const teachersListCursor = teachersCollection.find({ dept });
+    let teacherList = await teachersListCursor.toArray();
+    if (!teacherList) teacherList = [];
+
+    // getting current semester for that intake
+    const deptIntake = await departmentsCollection.findOne({
+      $and: [{ deptName: dept, intake }],
+    });
+
+    const completedSemester = deptIntake.completedSemester;
+
+    // finding subjects given dept and intake
+    const subjectsCursor = coursesCollection.find({
+      $and: [
+        { dept: dept.toLocaleLowerCase(), semester: completedSemester + 1 },
+      ],
+    });
+
+    let subjectList = await subjectsCursor.toArray();
+    if (!subjectList) subjectList = [];
+    res.send({ okay: true, data: { subjectList, teacherList } });
+  } catch (err) {
+    console.log(err);
+    res.send({ okay: false, msg: err });
+  }
 });
 
 module.exports = router;
